d75fa99638417d6676d93795184ad7a996269220
mikaeltreppo <mikaeltreppo@outlook.com>
2023-09-07 15:36:41 +0200

    Aggiungere i file di progetto.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    }
+
+    Console.WriteLine("]");
+
+};
+int Quadrato(int numero)
+{
+    int quadrato = numero * numero;
+    return quadrato;
+};
+
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayCopy = (int[])array.Clone();
+
+    for(int i = 0;i<arrayCopy.Length;i++ )
+    {
+        arrayCopy[i] = Quadrato(arrayCopy[i]);
+    }
+
+    return arrayCopy;
+    
+};
+int sommaElementiArray(int[] array)
+{
+    int sum = 0;
+    for(int i = 0;i<array.Length ; i++) {
+    
+    sum += array[i];
+    }
+    return sum;
+}
+
+//fine function d'appoggio
